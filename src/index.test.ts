8b32313ee149b96e6e575ae10e5e611c82c9d0b4
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2024-09-10 01:07:14 +0000

    use vitest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
-import { jest } from '@jest/globals'
+import { expect, describe, vi, test } from 'vitest'
 import * as main from './main.js'
 
 // Mock the action's entrypoint
-const runMock = jest.spyOn(main, 'run').mockImplementation(async () => {})
+const runMock = vi.spyOn(main, 'run').mockImplementation(async () => {})
 
 describe('index', () => {
-  it('calls run when imported', async () => {
+  test('calls run when imported', async () => {
     await import('./index.js')
     expect(runMock).toHaveBeenCalled()
   })
