5f7d6e2786fafc49c235d57abd4d951ff618891c
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2024-10-05 07:37:49 +0000

    fix errors when linting SHELL_SHFMT

--- examples/google-cloud/collector/deploy.sh
+++ examples/google-cloud/collector/deploy.sh
@@ -2,8 +2,8 @@
 
 PROJECT_ID='<your-project-id>'
 gcloud run deploy collector \
-  --project=${PROJECT_ID} \
-  --region=asia-northeast1 \
-  --source . \
-  --allow-unauthenticated \
-  --port=4318
+     --project=${PROJECT_ID} \
+     --region=asia-northeast1 \
+     --source . \
+     --allow-unauthenticated \
+     --port=4318
