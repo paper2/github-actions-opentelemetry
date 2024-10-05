89aca56d1a0816186eaad3ed95aa5c018f5891ba
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2024-10-05 07:29:58 +0000

    match calucuration to GitHub behavior

--- src/traces/github.ts
+++ src/traces/github.ts
@@ -6,25 +6,6 @@ import {
 } from '../github/index.js'
 import * as opentelemetry from '@opentelemetry/api'
 
-const tracer = opentelemetry.trace.getTracer(
-  'github-actions-opentelemetry-github'
-)
-
-const createSpan = (
-  ctx: Context,
-  name: string,
-  startedAt: string,
-  endAt: string,
-  attributes: opentelemetry.Attributes
-): opentelemetry.Span => {
-  const startTime = new Date(startedAt)
-  const endTime = new Date(endAt)
-
-  const span = tracer.startSpan(name, { startTime, attributes }, ctx)
-  span.end(endTime)
-  return span
-}
-
 export const createWorkflowRunTrace = (
   workflowRun: WorkflowRun,
   workflowRunJobs: WorkflowRunJobs
@@ -63,50 +44,36 @@ export const createWorkflowRunJobSpan = (
     return null
   }
 
-  const stepCompletedAtDates = job.steps.map(step =>
-    step.completed_at ? new Date(step.completed_at) : undefined
+  const spanWithWaiting = createSpan(
+    ctx,
+    `${job.name} with time of waiting runner`,
+    job.created_at,
+    job.completed_at,
+    // TODO: Set Attributes
+    {}
   )
-  const endAt = new Date(
-    Math.max(...stepCompletedAtDates.map(Number))
-  ).toISOString()
+  const ctxWithWaiting = opentelemetry.trace.setSpan(ctx, spanWithWaiting)
+
+  createWaitRunnerSpan(ctxWithWaiting, job)
 
   const span = createSpan(
-    ctx,
+    ctxWithWaiting,
     job.name,
-    job.created_at,
-    // Use last step completed_at insted of job.completed_at because last workflow job includes time of no running steps.
-    endAt,
+    job.started_at,
+    job.completed_at,
     // TODO: Set Attributes
     {}
   )
 
-  return opentelemetry.trace.setSpan(ctx, span)
+  return opentelemetry.trace.setSpan(ctxWithWaiting, span)
 }
 
 const createWaitRunnerSpan = (ctx: Context, job: WorkflowRunJob): void => {
-  if (job.steps === undefined || job.steps.length === 0) {
-    console.warn(`job (${job.name}) has no steps.`)
-    return
-  }
-
-  const setupStep = job.steps[0]
-  if (setupStep.name !== 'Set up job') {
-    console.warn(`This step is assumed "Set up job" but "${setupStep.name}".`)
-  }
-
-  if (setupStep.started_at == null || setupStep.completed_at == null) {
-    console.warn(
-      `step (${setupStep.name}) time is null|undifined. (started_at:${setupStep.started_at}, complated_at:${setupStep.completed_at})`
-    )
-    return
-  }
-
   createSpan(
     ctx,
     'Wait Runner',
     job.created_at,
-    // use 'Set up job' step's started_at becaues (job.started_at - job.created_at) includes it.
-    setupStep.started_at,
+    job.started_at,
     // TODO: Set Attributes
     {}
   )
@@ -121,8 +88,8 @@ export const createWorkflowRunStepSpan = (
     return
   }
 
-  createWaitRunnerSpan(ctx, job)
-
+  // NOTE: GitHub Action's first step (Set up job) is flaky. :(
+  // Sometimes it starts before job.started.
   job.steps.map(step => {
     if (step.started_at == null || step.completed_at == null) {
       console.warn(
@@ -139,4 +106,23 @@ export const createWorkflowRunStepSpan = (
       {}
     )
   })
+}
+
+const tracer = opentelemetry.trace.getTracer(
+  'github-actions-opentelemetry-github'
+)
+
+const createSpan = (
+  ctx: Context,
+  name: string,
+  startedAt: string,
+  endAt: string,
+  attributes: opentelemetry.Attributes
+): opentelemetry.Span => {
+  const startTime = new Date(startedAt)
+  const endTime = new Date(endAt)
+
+  const span = tracer.startSpan(name, { startTime, attributes }, ctx)
+  span.end(endTime)
+  return span
 }
