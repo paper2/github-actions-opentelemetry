1adc3fcabcf6b61cc396dbc25439b6fc614b67c0
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2024-11-10 05:51:57 +0000

    delete not verifying examples

--- examples/google-cloud/collector/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJECT_ID='<your-project-id>'
-gcloud run deploy collector \
-	--project=${PROJECT_ID} \
-	--region=asia-northeast1 \
-	--source . \
-	--allow-unauthenticated \
-	--port=4318
