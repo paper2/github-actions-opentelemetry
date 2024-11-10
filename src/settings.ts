e6595135ee9ffcb6d1560fa8d9984db4d8c91807
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2024-11-10 04:10:32 +0000

    add todo comment

--- src/settings.ts
+++ src/settings.ts
@@ -10,5 +10,8 @@ export const settings = {
   // Always set to true when GitHub Actions is running the workflow.
   isGitHubActions: process.env.GITHUB_ACTIONS === 'true'
 }
+// TODO: nodesdk利用していないので、dialogでデバックログ出せるように戻す
+// TODO: ACTIONS_RUNNER_DEBUGとACTIONS_STEP_DEBUGがTrueの時にdebug有効化するのも良いかも
+// https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/troubleshooting-workflows/enabling-debug-logging
 
 export default settings
