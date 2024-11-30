668d088224d35949a25e834a138b448ff4435d98
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2024-11-30 05:43:29 +0000

    remove @opentelemetry/auto-instrumentations-node

--- src/github/github.test.ts
+++ src/github/github.test.ts
@@ -2,8 +2,8 @@ import { describe, test, expect } from 'vitest'
 import { fetchWorkflowResults } from './github.js'
 
 describe('fetchWorkflowResults', () => {
-  // Tips: API limit is higher than non-authentication.
-  //       Authentication Command: $ export GITHUB_TOKEN=`gh auth token`
+  // Tips: If API limit exceed, authenticate by using below command
+  //       $ export GITHUB_TOKEN=`gh auth token`
   test('should fetch results using real api', async () => {
     await expect(fetchWorkflowResults(0, 1)).resolves.not.toThrow()
   })
