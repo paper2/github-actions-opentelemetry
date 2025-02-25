e9469e057c55fb41c342e54d17c07aa02e46fe15
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2025-02-25 23:17:53 +0000

    refactor: update test description for fetchWorkflowResults to clarify retry behavior

--- src/github/github.test.ts
+++ src/github/github.test.ts
@@ -5,8 +5,7 @@ describe('fetchWorkflowResults', () => {
   // Tips: If API limit exceed, authenticate by using below command
   //       $ export GITHUB_TOKEN=`gh auth token`
   test('should fetch results using real api', async () => {
+    // not test retry because it needs mock of checkCompleted but it affects correct test case.
     await expect(fetchWorkflowResults(0, 1)).resolves.not.toThrow()
   })
-
-  // not test retry because it needs mock of checkCompleted but it affects correct test case.
 })
